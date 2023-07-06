f4ee2578a4644196cb5f22aff8b7cda306351420
lary <34568834+heylary@users.noreply.github.com>
2023-07-06 23:18:40 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-mkdir -p build
-cd build
-cmake .. -DONNXRUNTIME_DIR=/home/narwal/Downloads/onnxruntime-linux-x64-1.12.1 # 将 /path/to/onnxruntime 替换为 ONNX Runtime 的安装路径
+
+build_dir="build"
+
+if [ ! -d "$build_dir" ]; then
+  mkdir -p "$build_dir"
+fi
+
+cd "$build_dir"
+cmake ..
 make
