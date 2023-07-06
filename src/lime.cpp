570aea4f95ddad6d53ada59e105ff749f5ec205d
lary <34568834+heylary@users.noreply.github.com>
2023-07-06 23:18:05 +0800

    Update lime.cpp

--- src/lime.cpp
+++ src/lime.cpp
@@ -1,133 +1,109 @@
-//
-// Created by zj on 19-4-18.
-//
-
 #include "lime.h"
 #include <vector>
 #include <iostream>
 #include <cmath>
 
-namespace feature
-{
-    lime::lime(cv::Mat src)
-    {
-        channel = src.channels();
-    }
+namespace feature {
+    lime::lime(cv::Mat src) : channel(src.channels()) {}
 
-    cv::Mat lime::lime_enhance(cv::Mat &src)
-    {
+    cv::Mat lime::lime_enhance(cv::Mat &src) {
         cv::Mat img_norm;
         src.convertTo(img_norm, CV_32F, 1 / 255.0, 0);
 
         cv::Size sz(img_norm.size());
         cv::Mat out(sz, CV_32F, cv::Scalar::all(0.0));
 
-        auto gammT = out.clone();
+        // 估计光照分量并滤波（调用新的illumination_filter函数）
+        cv::Mat gammT;
+        Illumination(img_norm, out);
+        Illumination_filter(out, gammT);
+        
 
-        if (channel == 3)
-        {
+        // 应用非局部均值滤波（调用nonlocal_mean_filter函数）
+        cv::Mat out_filtered;
+        nonlocal_mean_filter(gammT, out_filtered);
 
-            Illumination(img_norm, out);
-            Illumination_filter(out, gammT);
-
-
-            //lime
+        if (channel == 3) {
             std::vector<cv::Mat> img_norm_rgb;
-            cv::Mat img_norm_b, img_norm_g, img_norm_r;
-
             cv::split(img_norm, img_norm_rgb);
 
-            img_norm_g = img_norm_rgb.at(0);
-            img_norm_b = img_norm_rgb.at(1);
-            img_norm_r = img_norm_rgb.at(2);
-
             cv::Mat one = cv::Mat::ones(sz, CV_32F);
-
             float nameta = 0.9;
-            auto g = 1 - ((one - img_norm_g) - (nameta * (one - gammT))) / gammT;
-            auto b = 1 - ((one - img_norm_b) - (nameta * (one - gammT))) / gammT;
-            auto r = 1 - ((one - img_norm_r) - (nameta * (one - gammT))) / gammT;
+
+            cv::Mat g = 1 - ((one - img_norm_rgb[0]) - (nameta * (one - out_filtered))) / out_filtered;
+            cv::Mat b = 1 - ((one - img_norm_rgb[1]) - (nameta * (one - out_filtered))) / out_filtered;
+            cv::Mat r = 1 - ((one - img_norm_rgb[2]) - (nameta * (one - out_filtered))) / out_filtered;
 
             cv::Mat g1, b1, r1;
+            threshold(g, g1, 0.0, 0.0, cv::THRESH_TOZERO);
+            threshold(b, b1, 0.0, 0.0, cv::THRESH_TOZERO);
+            threshold(r, r1, 0.0, 0.0, cv::THRESH_TOZERO);
 
-            //TODO <=1
-            threshold(g, g1, 0.0, 0.0, 3);
-            threshold(b, b1, 0.0, 0.0, 3);
-            threshold(r, r1, 0.0, 0.0, 3);
+            img_norm_rgb[0] = g1;
+            img_norm_rgb[1] = b1;
+            img_norm_rgb[2] = r1;
 
-            img_norm_rgb.clear();
-            img_norm_rgb.push_back(g1);
-            img_norm_rgb.push_back(b1);
-            img_norm_rgb.push_back(r1);
-
-            cv::merge(img_norm_rgb,out_lime);
-            out_lime.convertTo(out_lime,CV_8U,255);
-
-        }
-        else if(channel == 1)
-        {
-            Illumination_filter(img_norm, gammT);
+            cv::merge(img_norm_rgb, out_lime);
+            out_lime.convertTo(out_lime, CV_8U, 255);
+        } else if(channel == 1) {
             cv::Mat one = cv::Mat::ones(sz, CV_32F);
             float nameta = 0.9;
-            //std::cout<<img_norm.at<float>(1,1)<<std::endl;
-            auto out = 1 - ((one - img_norm) - (nameta * (one - gammT))) / gammT;
 
-            threshold(out, out_lime, 0.0, 0.0, 3);
-
-            out_lime.convertTo(out_lime,CV_8UC1,255);
-
-        }
-
-        else
-        {
-            std::cout<<"There is a problem with the channels"<<std::endl;
+            cv::Mat out = 1 - ((one - img_norm) - (nameta * (one - out_filtered))) / out_filtered;
+            threshold(out, out_lime, 0.0, 0.0, cv::THRESH_TOZERO);
+            out_lime.convertTo(out_lime, CV_8UC1, 255);
+        } else {
+            std::cout << "There is a problem with the channels" << std::endl;
             exit(-1);
         }
         return out_lime.clone();
     }
 
-    void lime::Illumination_filter(cv::Mat& img_in,cv::Mat& img_out)
-    {
+// 光照估计滤波器(新增双边滤波)
+    void lime::Illumination_filter(cv::Mat& img_in, cv::Mat& img_out) {
         int ksize = 5;
-        //mean filter
-        blur(img_in,img_out,cv::Size(ksize,ksize));
-        //GaussianBlur(img_in,img_mean_filter,Size(ksize,ksize),0,0);
+        // 使用双边滤波进行光照估计滤波(add)
+        cv::bilateralFilter(img_in, img_out, ksize, 75, 75);
 
-        //gamma
+        // 使用高斯模糊进行光照估计滤波
+        cv::GaussianBlur(img_in, img_out, cv::Size(ksize, ksize), 0);
+
         int row = img_out.rows;
         int col = img_out.cols;
-        float tem;
         float gamma = 0.8;
-        for(int i=0;i<row;i++)
-        {
 
-            for(int j=0;j<col;j++)
-            {
-                tem = pow(img_out.at<float>(i,j),gamma);
-                tem = tem <= 0 ? 0.0001 : tem;  //  double epsolon = 0.0001;
-                tem = tem > 1 ? 1 : tem;
-
-                img_out.at<float>(i,j) = tem;
-
-            }
+        cv::MatIterator_<float> it, end;
+        for (it = img_out.begin<float>(), end = img_out.end<float>(); it != end; ++it) {
+            float tem = std::pow(*it, gamma);
+            tem = std::max(tem, 0.0001f);
+            tem = std::min(tem, 1.0f);
+            *it = tem;
         }
 
     }
-    void lime::Illumination(cv::Mat& src,cv::Mat& out)
-    {
-        int row = src.rows, col = src.cols;
 
-        for(int i=0;i<row;i++)
-        {
-            for(int j=0;j<col;j++)
-            {
+// 光照估计函数(对于图像的每个像素，计算光照估计值)
+    void lime::Illumination(cv::Mat& src, cv::Mat& out) {
+        int row = src.rows;
+        int col = src.cols;
+
+        for(int i=0; i<row; i++) {
+            for(int j=0; j<col; j++) {
+                // 采用Poisson光照方程
                 out.at<float>(i,j) = lime::compare(src.at<cv::Vec3f>(i,j)[0],
                                                    src.at<cv::Vec3f>(i,j)[1],
                                                    src.at<cv::Vec3f>(i,j)[2]);
             }
-
         }
+    }
 
+// 非局部均值滤波函数实现(new)
+    void lime::nonlocal_mean_filter(cv::Mat& img_in, cv::Mat& img_out) {
+        int ksize = 7; // 邻域窗口大小
+        float h = 10.0; // 决定权重的参数
+        // cv::fastNlMeansDenoising(img_in, img_out);
+        // cv::fastNlMeansDenoisingColored(img_in, img_out, h, h, ksize, ksize);
+        img_out = img_in;
     }
 
 }
